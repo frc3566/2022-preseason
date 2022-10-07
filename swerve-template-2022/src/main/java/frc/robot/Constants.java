0fcb7b50035446d0ba0bcb4fc5465518a88d3351
zzpDavid2 <51421093+zzpDavid2@users.noreply.github.com>
2022-10-07 17:55:08 -0400

    Fixed a Major Oversight that the wrong module model is used & calibrated BackLeft and BackRight Modules
    However the hardware is not build to spec so that the modules are having trouble aligning themselves.

--- swerve-template-2022/src/main/java/frc/robot/Constants.java
+++ swerve-template-2022/src/main/java/frc/robot/Constants.java
@@ -41,10 +41,10 @@ public final class Constants {
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7; // FIXME Set back left drive motor ID
     public static final int BACK_LEFT_MODULE_STEER_MOTOR = 8; // FIXME Set back left steer motor ID
     public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; // FIXME Set back left steer encoder ID
-    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+    public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(187.5); // FIXME Measure and set back left steer offset
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10; // FIXME Set back right drive motor ID
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 11; // FIXME Set back right steer motor ID
     public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12; // FIXME Set back right steer encoder ID
-    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
+    public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(45.8); // FIXME Measure and set back right steer offset
 }

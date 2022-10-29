a4d8eb374227dc18306161c6f2774eedb0cc35cb
Kaien <kaienhung999@gmail.com>
2022-10-28 23:05:35 -0400

    Constants/PID
    Edited Constants for accuracy, changed PID slightly (Kp from 0.2 to 0.225, Kd from 0.1 to 0.115)

--- swerve-template-2022/src/main/java/com/swervedrivespecialties/swervelib/Mk4iSwerveModuleHelper.java
+++ swerve-template-2022/src/main/java/com/swervedrivespecialties/swervelib/Mk4iSwerveModuleHelper.java
@@ -20,7 +20,7 @@ public final class Mk4iSwerveModuleHelper {
     private static SteerControllerFactory<?, Falcon500SteerConfiguration<CanCoderAbsoluteConfiguration>> getFalcon500SteerFactory(Mk4ModuleConfiguration configuration) {
         return new Falcon500SteerControllerFactoryBuilder()
                 .withVoltageCompensation(configuration.getNominalVoltage())
-                .withPidConstants(0.2, 0.0, 0.1)
+                .withPidConstants(0.225, 0.0, 0.115)
                 .withCurrentLimit(configuration.getSteerCurrentLimit())
                 .build(new CanCoderFactoryBuilder()
                         .withReadingUpdatePeriod(100)
